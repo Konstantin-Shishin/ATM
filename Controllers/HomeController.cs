90074ff9075ad9643c1b8c0a8297f8f06eac8829
Konstantin_Shishin <shishinkostya2000@gmail.com>
2021-07-13 13:46:05 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ATMApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATMApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        ATM ATM = new ATM();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ATM);
+        }
+        [HttpPost]
+        public IActionResult GiveOut(int GiveOutSum, int Nominal)
+        {
+            if (GiveOutSum == 100)
+            {
+                ATM.bills[50] = ATM.bills[50] - 2;
+            }
+            return View(ATM);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ATM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ATMApp.Models;
+
+
+namespace ATMApp.Models
+{
+    public class ATM
+    {
+        public int CountMaxBills = 1000;
+        public Dictionary<int, int> bills;
+
+        public ATM()
+        {
+            bills = new Dictionary<int, int>(7);
+            bills.Add(10,101);
+            bills.Add(50, 100);
+            bills.Add(100, 100);
+            bills.Add(200, 100);
+            bills.Add(500, 100);
+            bills.Add(1000, 100);
+            bills.Add(5000, 100);
+        }
+
+        public void GetBills()
+        {
+            return;
+        }
+        public void GiveOutBills()
+        {
+            return;
+        }
+
+
+
+    }
+}
